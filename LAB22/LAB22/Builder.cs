73af1e98fb6fce7fd8d36ab370a9a435e74137e4
Dingo-K <denis49232@mail.ru>
2021-03-26 00:02:15 +0300

    more

---
+++ LAB22/LAB22/Builder.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LAB22
+{
+    class StudentBuilder
+    {
+        public FIO FIO;
+        public Ball Ball;
+        public Place Place;
+        public Aboutgroup Aboutgroup;
+
+        public StudentBuilder(string firstname, string name, string secondname, string special, int course, string gender, int group, double ball, string city, string street, string build, string flat)
+        {
+            FIO.firstname = firstname;
+            FIO.name = name;
+            FIO.secondname = secondname;
+            Ball.ball = ball;
+            Place.city = city;
+            Place.build = build;
+            Place.street = street;
+            Place.flat = flat;
+            Aboutgroup.course = course;
+            Aboutgroup.group = group;
+            Aboutgroup.special = special;
+        }
+    }
+
+
+    class FIO
+    {
+        public string firstname;
+        public string name;
+        public string secondname;
+        public string gender;
+    }
+
+    class Ball
+    {
+        public double ball;
+    }
+
+    class Place
+    {
+        public string city;
+        public string build;
+        public string street;
+        public string flat;
+    }
+
+    class Aboutgroup
+    {
+        public int group;
+        public int course;
+        public string special;
+    }
+}
---
+++ LAB22/LAB22/Faztory.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LAB22
+{
+    class Faztory
+    {
+    }
+
+   abstract class Adding
+    {
+        public abstract void Add(string firstname, string name, string secondname, string special, int course, string gender, string time, int group, double ball, string city, string street, string build, string flat);
+    }
+
+    class Create : Adding
+    {
+        public string name;    
+        public string firstname;
+        public string secondname;
+        public string special;
+        public int course;
+        public string gender;
+        public string time;
+        public int group;
+        public double ball;
+        public string city;
+        public string street;
+        public string build;
+        [Valid]
+        public string flat;
+
+        public override void Add(string firstname, string name, string secondname, string special, int course, string gender, string time, int group, double ball, string city, string street, string build, string flat)
+        {
+            this.name = name;
+            this.firstname = firstname;
+            this.secondname = secondname;
+            this.course = course;
+            this.special = special;
+            this.gender = gender;
+            this.time = time;
+            this.group = group;
+            this.ball = ball;
+            this.city = city;
+            this.street = street;
+            this.build = build;
+            this.flat = flat;
+
+        }
+    }
+    
+    abstract class FactoryStudent
+    {
+        public abstract Adding Create_student();
+    }
+
+    class StudentFactory : FactoryStudent
+    {
+        
+        public override Adding Create_student()
+        {
+            return new Create();
+        }
+    }
+    
+    class Students1
+    {
+        public string name;
+        public string firstname;
+        public string secondname;
+        public string special;
+        public int course;
+        public string gender;
+        public string time;
+        public int group;
+        public double ball;
+        public string city;
+        public string street;
+        public string build;
+        [Valid]
+        public string flat;
+        private Adding Create;
+
+        public Students1(StudentFactory factory)
+        {
+            Create = factory.Create_student();
+        }
+
+        public void Add()
+        {
+            Create.Add(firstname, name, secondname, special, course, gender, time, group, ball, city, street, build, flat);
+        }
+    }
+}
