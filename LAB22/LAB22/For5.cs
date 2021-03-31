fecbad503a3aabef94480bc90c17e1cb38f5d159
Dingo-K <denis49232@mail.ru>
2021-03-31 19:17:49 +0300

    new

---
+++ LAB22/LAB22/For5.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace LAB22
+{ // паттерн Command
+    interface ISerial
+    {
+        void execute(Students students);
+    }
+    class Serial // получатель
+    {
+
+        public void XMLSerialize(Students students)
+        {
+            XmlSerializer xml = new XmlSerializer(typeof(Students));
+
+            using (FileStream fs = new FileStream("StudentsSort.xml", FileMode.OpenOrCreate))
+            {
+                xml.Serialize(fs, students);
+            }
+        }
+    }
+
+    class SerCommand : ISerial // конкретная команда
+    {
+        Serial Serial;
+        public SerCommand(Serial serial_)
+        {
+            Serial = serial_;
+        }
+        public void execute(Students students)
+        {
+            Serial.XMLSerialize(students);
+        }
+    }
+
+    class Into // инициатор
+    {
+        ISerial Serial;
+        public Into()
+        {
+
+        }
+
+        public void SetCommand(ISerial serial_)
+        {
+            Serial = serial_;
+        }
+
+        public void Enter(Students students)
+        {
+            Serial.execute(students);
+        }
+    }
+}
--- LAB22/LAB22/Student.cs
+++ LAB22/LAB22/Student.cs
@@ -6,33 +6,50 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace LAB22
-{
+{ // паттерн адаптер
+    public interface IFstudent
+    {
+        void ReadTo();
+    }
+
+    public interface IWorker
+    {
+        string name { get; set; }
+        string secondname { get; set; }
+        string position { get; set; }
+        
+
+        
+    }
 
      public interface Istudent
     {
         Istudent Clone();
     }
     [Serializable]
-    public class Student 
+    public class Student : IFstudent
     {
-        public string name;
+        public string name { get; set; }
         [Required(ErrorMessage = "Фамилия не установлена")]
-        public string firstname;
-        public string secondname;
-        public string special;
-        public int course;
-        public string gender;
-        public string time;
-        public int group;
+        public string firstname { get; set; }
+        public string secondname { get; set; }
+        public string special { get; set; }
+        public int course { get; set; }
+        public string gender { get; set; }
+        public string time { get; set; }
+        public int group { get; set; }
         [Range(0, 100, ErrorMessage = "Недопустимый балл")]
-        public double ball;
-        public string city;
-        public string street;
-        public string build;
+        public double ball { get; set; }
+        public string city { get; set; }
+        public string street { get; set; }
+        public string build { get; set; }
         [Valid]
-        public string flat;
+        public string flat { get; set; }
 
-       
+        public void ReadTo()
+        {
+
+        }
         public Student()
         {
 
@@ -53,6 +70,43 @@ namespace LAB22
             this.street = street;
             this.build = build;
             this.flat = flat;
+        }
+    }
+
+    public class Worker : IWorker
+    {
+        public string name { get; set; }
+        public string secondname { get; set; }
+        
+        public string position { get; set; }
+        
+    }
+
+    public class AboutpersonAdapt : IFstudent
+    {
+        Worker worker;
+        Student Student;
+
+        public AboutpersonAdapt(Worker _worker)
+        {
+            worker = _worker;
+        }
+
+        public AboutpersonAdapt(Student _Student)
+        {
+            Student = _Student;
+        }
+
+        public void ReadTo()
+        {
+            if(worker != null)
+            {
+                Console.WriteLine(worker.name + "\n" + worker.secondname + "\n" + worker.position);
+            }
+            if(Student != null)
+            {
+                Console.WriteLine(Student.name + "\n" + Student.secondname + "\n" + Student.special);
+            }
         }
     }
     [Serializable]
